258abb8b64a7ad0a6a2fdd1e37570654dd1a8786
CyM <tina2013miao@gmail.com>
2026-07-24 19:06:41 +0800

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,17 @@
-// Wathin｜未止 — _document.tsx  v2.0.0
+// Wathin｜未止 — _document.tsx
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="zh-TW">
-      <Head />
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, viewport-fit=cover"
+        />
+        <meta name="theme-color" content="#020911" />
+      </Head>
+
       <body>
         <Main />
         <NextScript />
